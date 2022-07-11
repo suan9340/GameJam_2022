aab59f60fa38a3a6a4b5edefaf90f281f43fa3c8
SeoHyun <namja4097@gmail.com>
2022-07-11 20:13:09 +0900

    랜덤생성 근데 확률조작을 가미한

--- Assets/02_Scripts/Enemy/EnemySpawn.cs
+++ Assets/02_Scripts/Enemy/EnemySpawn.cs
@@ -7,29 +7,56 @@ public class EnemySpawn : MonoBehaviour
     public bool enableSpawn = false;
 
     public GameObject Enemy;
+
+    public GameObject[] spawnPoint;
     void Start()
     {
         enableSpawn = true;
-        InvokeRepeating("SpawnEnemy", 3, 1); 
+        InvokeRepeating("SpawnEnemy", 3,2.5f); 
         
     }
 
-    void Update()
-    {
-
-    }
 
   
 
     void SpawnEnemy()
     {
-        float randomX = Random.Range(-2.5f, 2.5f);
-        float randomY = Random.Range(-5f, 5f);
-        if (enableSpawn)
+        float randomX = Random.Range(-3,  4);
+        float randomY = Random.Range(-5, 6);
+
+        float random1 = Random.Range(-3, -4);
+        float random2 = Random.Range(3, 4);
+
+        float randomx = Random.Range(-3, 4);
+        float randomy = Random.Range(-6,6);
+
+        float random4 = Random.Range(-5,-6);
+        float random6 = Random.Range(5,6);
+
+        if(randomX <0 &&randomY<0)
         {
-            GameObject enemy = (GameObject)Instantiate(Enemy, new Vector3(randomX, randomY, 0f), Quaternion.identity); 
+        GameObject enemy =(GameObject)Instantiate(Enemy, new Vector3(randomx, random4, 0f), Quaternion.identity);
+
+        }if(randomX >0 &&randomY>0)
+        {
+        GameObject enemy =(GameObject)Instantiate(Enemy, new Vector3(random2, randomy, 0f), Quaternion.identity);
+
+        }if(randomX <0 &&randomY>0)
+        {
+        GameObject enemy =(GameObject)Instantiate(Enemy, new Vector3(random1, randomy, 0f), Quaternion.identity);
+
+        }if(randomX >0 &&randomY<0)
+        {
+        GameObject enemy =(GameObject)Instantiate(Enemy, new Vector3(randomx, random6, 0f), Quaternion.identity);
+
         }
+
+
+
+      
+
     }
 
-  
+
+
 }
