4fa966a11ec64d51b1755b3a30de24ce3832a54f
suan9340 <sue9340@gmail.com>
2022-07-11 23:54:07 +0900

    아이템 구조

--- Assets/02_Scripts/01_Utils/ConstantManager.cs
+++ Assets/02_Scripts/01_Utils/ConstantManager.cs
@@ -3,6 +3,7 @@ public static class ConstantManager
     public const string TAG_PLAYER = "PLAYER";
     public const string TAG_BULLET = "BULLET";
     public const string TAG_ENEMY = "ENEMY";
+    public const string TAG_ITEM = "ITEM";
 
 
     public const string INPUT_HO = "Horizontal";
---
+++ Assets/02_Scripts/Item/Item3.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item3 : MonoBehaviour, Item_Interface
+{
+    public void Ability()
+    {
+
+    }
+}
