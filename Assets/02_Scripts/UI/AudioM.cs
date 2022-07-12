f8fa6866d1eccb8e7e4c00558470f59d8e249bfe
SeoHyun <namja4097@gmail.com>
2022-07-12 13:46:52 +0900

    마스터볼륨 효과음 추가

--- Assets/02_Scripts/UI/AudioM.cs
+++ Assets/02_Scripts/UI/AudioM.cs
@@ -11,31 +11,30 @@ public class AudioM : MonoBehaviour
     public AudioMixer BGMMixer;
     public AudioMixer MasterMixer;
     public AudioMixer SFXMixer;
-    public Slider audioSlider;
+    public Slider BGMSlider;
+    public Slider MasterSlider;
+    public Slider SFXSlider;
 
-   //public void SetLevel(float sliderval)
-   // {
-   //     BGMMixer.SetFloat("BGM",Mathf.Log10(sliderval)*20);
-   // }
-
-    public void BGMControl()
-    {
-        float sound = audioSlider.value;
-        if (sound == -40f) BGMMixer.SetFloat("BGM", -80);
-        else BGMMixer.SetFloat("BGM", sound);
-    }
-
+  
     public void MasterControl()
     {
-        float sound = audioSlider.value;
+        float sound = MasterSlider.value;
         if (sound == -40f) MasterMixer.SetFloat("Master", -80);
         else MasterMixer.SetFloat("Master", sound);
     
     }  
+
+    public void BGMControl()
+    {
+        float sound = BGMSlider.value;
+        if (sound == -40f) BGMMixer.SetFloat("BGM", -80);
+        else BGMMixer.SetFloat("BGM", sound);
+    }
+
     
     public void SFXControl()
     {
-        float sound = audioSlider.value;
+        float sound = SFXSlider.value;
         if (sound == -40f) SFXMixer.SetFloat("SFX", -80);
         else SFXMixer.SetFloat("SFX", sound);
     
