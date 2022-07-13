d5bb3a5bc22cf111dac97ceeb210472cab4f1e95
SeoHyun <namja4097@gmail.com>
2022-07-13 14:50:56 +0900

    효과음 함수 정리

--- Assets/02_Scripts/01_Utils/AudioManager.cs
+++ Assets/02_Scripts/01_Utils/AudioManager.cs
@@ -7,13 +7,35 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+
+    #region Singleton
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<AudioManager>();
+                if (_instance == null)
+                {
+                    _instance = new GameObject("AudioManager").AddComponent<AudioManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+    private static AudioManager _instance = null;
+
     public AudioSource BGM;
     public AudioSource FX;
     public Slider MasterSlider;
     public Slider BGMSlider;
     public Slider FXSlider;
     public List<AudioClip> music = new List<AudioClip>();
+    public List<AudioClip> vfx = new List<AudioClip>();
     private float backVol = 1;
+    #endregion
+
 
 
     void Start()
@@ -58,4 +80,44 @@ public class AudioManager : MonoBehaviour
         BGM.clip = music[Random.Range(0, music.Count)];
         BGM.Play();
     }
+
+
+    // 오디오 함수에 들어가 있으니까 필요한곳에 참조해서  FX.Play();[유니티 내장 함수] 하면 실행됨 
+    public void EnemyDieSound()
+    {
+        FX.clip = vfx[0];
+        FX.Play();
+    }
+
+
+    public void GunSound()
+    {
+        FX.clip = vfx[1];
+        FX.Play();
+        Debug.Log("뿡뿡띠");
+    }
+
+    public void PlayerDieSound()
+    {
+        FX.clip = vfx[2];
+        FX.Play();
+    }
+    
+    public void ItemAcquisition()
+    {
+        FX.clip = vfx[3];
+        FX.Play();
+    }
+
+    public void OnClickButton()
+    {
+        FX.clip = vfx[4];
+        FX.Play();
+    }
+
+    
+
 }
+   
+
+
