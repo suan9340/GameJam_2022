49f562a21c2f3b7a455e29e63d40065a86517f84
SeoHyun <namja4097@gmail.com>
2022-07-15 09:29:20 +0900

    오디오수쩡

---
+++ Assets/02_Scripts/MenuS.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuS : MonoBehaviour
+{
+   public void StartB()
+    {
+        AudioManager.Instance.OnClickStart();
+    }
+}
